5b958272861d20e4092be4fb4b19ee9cff684011
dani-ribeiro <10DanielRibeiro@gmail.com>
2024-04-03 18:54:02 -0500

    Rubric Approved

---
+++ server/server.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const apicache = require('apicache');
+const cache = apicache.middleware;
+
+// import apicache from 'apicache'
+const PORT = 3456;
+
+// when client sends a GET request is made to /api --> send json
+// app.get('/api', (req, res) => {
+//     res.json({'users': ['userOne', 'userTwo', 'userThree', 'userFour']});
+// });
+
+const options = {
+    method: 'GET',
+    headers: {
+        'x-apisports-host': 'v3.football.api-sports.io',
+        'x-apisports-key': process.env.API_KEY2
+    },
+};
+
+// rather than doing /api route --> do /(whatever endpoint)?(variable1=value1)&(...)
+// that way I can parse it here and fetch it with the values!!!!
+app.get('/api/standings/:leagueID/:seasonYEAR', cache('24 hours'), (req, res) => {
+    const { leagueID, seasonYEAR } = req.params;
+    fetch(`https://v3.football.api-sports.io/standings?league=${leagueID}&season=${seasonYEAR}`, options)
+    .then(response => {
+        if(!response.ok){
+            throw new Error("ERROR: Bad Response");
+        }else{
+            return response.json();
+        }
+    })
+    .then(data => {
+        if(data.response && data.response.length > 0){
+            // parse the response and send ONLY relevant data to client
+            const league = data.response[0].league;
+            if(league.standings){
+                const standingsData = league.standings[0];
+                const standings = {};    // dictionary mapping <Team, Team Information>
+
+                /* team name (string): Team's Name  maps to     {   rank (int):              League position
+                                                                    matches_played (int):    Total matches played
+                                                                    wins (int):              Total matches won
+                                                                    draws (int):             Total matches drawn
+                                                                    losses (int):            Total matches lost
+                                                                    goals_for (int):         Total goals against other teams
+                                                                    goals_conceded (int):    Total goals scored against this team
+                                                                    goal_difference (int):   Goal difference (goals_conceded - goals_for)
+                                                                    points (int):            Total league points
+                                                                    form (string):           Represents last 5 game record (i.e. WWLLD = win, win, lose, lose, draw)
+                                                                }
+                */
+
+                for(const currentTeam of standingsData){
+                    standings[currentTeam.team.name] =  {
+                                                            rank:               currentTeam.rank,
+                                                            matches_played:     currentTeam.all.played,
+                                                            wins:               currentTeam.all.win,
+                                                            draws:              currentTeam.all.played,
+                                                            losses:             currentTeam.all.lose,
+                                                            goals_for:          currentTeam.all.goals.for,
+                                                            goals_conceded:     currentTeam.all.goals.against,
+                                                            goals_difference:   currentTeam.goalsDiff,
+                                                            points:             currentTeam.points,
+                                                            form:               currentTeam.form,
+                                                        }
+                }
+                res.json(standings);
+            }else{
+                console.log(league);
+                // no standings available
+                res.json({});
+            }
+        }else{
+            console.log(data);
+            // no response
+            res.json({});
+        }
+    })
+    .catch(error => console.error(error));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
